4a8b75ac902c5bd70db274ea2b92eeed073b4e93
Fabian Mundt <f.mundt@inventionate.de>
2020-07-02 10:44:47 +0200

    Improve scalebar unit display

--- R/utilities.R
+++ R/utilities.R
@@ -586,11 +586,33 @@
 
 # scalebar
 # From https://github.com/oswaldosantos/ggsn/blob/master/R/scalebar.R
-scalebar <- function(data = NULL, location = "bottomright", dist = NULL, dist_unit = NULL, transform = NULL, dd2km = NULL, model = NULL, height = 0.02, st.dist = 0.02, st.bottom = TRUE, st.size = 5, st.color = "black", box.fill = c("black", "white"), box.color = "black", border.size = 1, x.min = NULL, x.max = NULL, y.min = NULL, y.max = NULL, anchor = NULL, facet.var = NULL, facet.lev = NULL, st.inherit = TRUE){
+scalebar <- function(data = NULL,
+                     location = "bottomright",
+                     dist = NULL,
+                     dist_unit = NULL,
+                     transform = NULL,
+                     dd2km = NULL,
+                     model = NULL,
+                     height = 0.02,
+                     st.dist = 0.02,
+                     st.bottom = TRUE,
+                     st.size = 5,
+                     st.color = "black",
+                     box.fill = c("black", "white"),
+                     box.color = "black",
+                     border.size = 1,
+                     x.min = NULL,
+                     x.max = NULL,
+                     y.min = NULL,
+                     y.max = NULL,
+                     anchor = NULL,
+                     facet.var = NULL,
+                     facet.lev = NULL,
+                     st.inherit = TRUE) {
   if (is.null(data)) {
     if (is.null(x.min) | is.null(x.max) |
-        is.null(y.min) | is.null(y.max) ) {
-      stop('If data is not defined, x.min, x.max, y.min and y.max must be.')
+      is.null(y.min) | is.null(y.max)) {
+      stop("If data is not defined, x.min, x.max, y.min and y.max must be.")
     }
     data <- data.frame(long = c(x.min, x.max), lat = c(y.min, y.max))
   }
@@ -612,49 +634,45 @@ scalebar <- function(data = NULL, location = "bottomright", dist = NULL, dist_un
       stop("'", substitute(data), "' must have columns with names that start with 'lat' and 'long'")
     }
   }
-  if (location == 'bottomleft') {
+  if (location == "bottomleft") {
     if (is.null(anchor)) {
       x <- xmin
       y <- ymin
     } else {
-      x <- as.numeric(anchor['x'])
-      y <- as.numeric(anchor['y'])
+      x <- as.numeric(anchor["x"])
+      y <- as.numeric(anchor["y"])
     }
     direction <- 1
-
   }
-  if (location == 'bottomright') {
+  if (location == "bottomright") {
     if (is.null(anchor)) {
       x <- xmax
       y <- ymin
     } else {
-      x <- as.numeric(anchor['x'])
-      y <- as.numeric(anchor['y'])
+      x <- as.numeric(anchor["x"])
+      y <- as.numeric(anchor["y"])
     }
     direction <- -1
-
   }
-  if (location == 'topleft') {
+  if (location == "topleft") {
     if (is.null(anchor)) {
       x <- xmin
       y <- ymax
     } else {
-      x <- as.numeric(anchor['x'])
-      y <- as.numeric(anchor['y'])
+      x <- as.numeric(anchor["x"])
+      y <- as.numeric(anchor["y"])
     }
     direction <- 1
-
   }
-  if (location == 'topright') {
+  if (location == "topright") {
     if (is.null(anchor)) {
       x <- xmax
       y <- ymax
     } else {
-      x <- as.numeric(anchor['x'])
-      y <- as.numeric(anchor['y'])
+      x <- as.numeric(anchor["x"])
+      y <- as.numeric(anchor["y"])
     }
     direction <- -1
-
   }
   if (!st.bottom) {
     st.dist <-
@@ -676,16 +694,24 @@ scalebar <- function(data = NULL, location = "bottomright", dist = NULL, dist_un
     cat("dd2km is deprecated. Use ggsn::transform instead.")
   }
   if (transform) {
-    break1 <- maptools::gcDestination(lon = x, lat = y,
-                                      bearing = 90 * direction,
-                                      dist = dist, dist.units = dist_unit,
-                                      model = model)[1, 1]
-    break2 <- maptools::gcDestination(lon = x, lat = y,
-                                      bearing = 90 * direction,
-                                      dist = dist*2, dist.units = dist_unit,
-                                      model = model)[1, 1]
+    break1 <- maptools::gcDestination(
+      lon = x,
+      lat = y,
+      bearing = 90 * direction,
+      dist = dist,
+      dist.units = dist_unit,
+      model = model
+    )[1, 1]
+    break2 <- maptools::gcDestination(
+      lon = x,
+      lat = y,
+      bearing = 90 * direction,
+      dist = dist * 2,
+      dist.units = dist_unit,
+      model = model
+    )[1, 1]
   } else {
-    if (location == 'bottomleft' | location == 'topleft') {
+    if (location == "bottomleft" | location == "topleft") {
       if (exists("dist_unit0") | (!exists("dist_unit0") & dist_unit == "km")) {
         break1 <- x + dist * 1e3
         break2 <- x + dist * 2e3
@@ -714,7 +740,6 @@ scalebar <- function(data = NULL, location = "bottomright", dist = NULL, dist_un
         break2 <- x - dist
       }
     }
-
   }
 
   out_of_range <- function(low, n, high) {
@@ -722,36 +747,51 @@ scalebar <- function(data = NULL, location = "bottomright", dist = NULL, dist_un
   }
 
   if (out_of_range(xmin, break1, xmax) | out_of_range(xmin, break2, xmax)) {
-    stop("The requested scalebar distance (",
-         substitute(dist), " ", substitute(dist_unit),
-         ") is too large to fit on the map.\n  Try reducing it.")
+    stop(
+      "The requested scalebar distance (",
+      substitute(dist), " ", substitute(dist_unit),
+      ") is too large to fit on the map.\n  Try reducing it."
+    )
   }
 
 
-  box1 <- data.frame(x = c(x, x, rep(break1, 2), x),
-                     y = c(y, height, height, y, y), group = 1)
-  box2 <- data.frame(x = c(rep(break1, 2), rep(break2, 2), break1),
-                     y=c(y, rep(height, 2), y, y), group = 1)
+  box1 <- data.frame(
+    x = c(x, x, rep(break1, 2), x),
+    y = c(y, height, height, y, y),
+    group = 1
+  )
+  box2 <- data.frame(
+    x = c(rep(break1, 2), rep(break2, 2), break1),
+    y = c(y, rep(height, 2), y, y),
+    group = 1
+  )
   if (!is.null(facet.var) & !is.null(facet.lev)) {
-    for (i in 1:length(facet.var)){
+    for (i in 1:length(facet.var)) {
       if (any(class(data) == "sf")) {
-        if (!is.factor(data[ , facet.var[i]][[1]])) {
-          data[ , facet.var[i]] <- factor(data[ , facet.var[i]][[1]])
+        if (!is.factor(data[, facet.var[i]][[1]])) {
+          data[, facet.var[i]] <- factor(data[, facet.var[i]][[1]])
         }
-        box1[ , facet.var[i]] <- factor(facet.lev[i],
-                                        levels(data[ , facet.var[i]][[1]]))
-        box2[ , facet.var[i]] <- factor(facet.lev[i],
-                                        levels(data[ , facet.var[i]][[1]]))
+        box1[, facet.var[i]] <- factor(
+          facet.lev[i],
+          levels(data[, facet.var[i]][[1]])
+        )
+        box2[, facet.var[i]] <- factor(
+          facet.lev[i],
+          levels(data[, facet.var[i]][[1]])
+        )
       } else {
-        if (!is.factor(data[ , facet.var[i]])) {
-          data[ , facet.var[i]] <- factor(data[ , facet.var[i]])
+        if (!is.factor(data[, facet.var[i]])) {
+          data[, facet.var[i]] <- factor(data[, facet.var[i]])
         }
-        box1[ , facet.var[i]] <- factor(facet.lev[i],
-                                        levels(data[ , facet.var[i]]))
-        box2[ , facet.var[i]] <- factor(facet.lev[i],
-                                        levels(data[ , facet.var[i]]))
+        box1[, facet.var[i]] <- factor(
+          facet.lev[i],
+          levels(data[, facet.var[i]])
+        )
+        box2[, facet.var[i]] <- factor(
+          facet.lev[i],
+          levels(data[, facet.var[i]])
+        )
       }
-
     }
   }
   if (exists("dist_unit0")) {
@@ -759,40 +799,56 @@ scalebar <- function(data = NULL, location = "bottomright", dist = NULL, dist_un
   } else {
     legend <- cbind(text = c(0, dist, dist * 2), row.names = NULL)
   }
-  gg.box1 <- geom_polygon(data = box1, aes(x, y),
-                          fill = utils::tail(box.fill, 1),
-                          color = utils::tail(box.color, 1),
-                          size = border.size)
-  gg.box2 <- geom_polygon(data = box2, aes(x, y), fill = box.fill[1],
-                          color = box.color[1],
-                          size = border.size)
+  gg.box1 <- geom_polygon(
+    data = box1, aes(x, y),
+    fill = utils::tail(box.fill, 1),
+    color = utils::tail(box.color, 1),
+    size = border.size
+  )
+  gg.box2 <- geom_polygon(
+    data = box2, aes(x, y), fill = box.fill[1],
+    color = box.color[1],
+    size = border.size
+  )
   x.st.pos <- c(box1[c(1, 3), 1], box2[3, 1])
-  if (location == 'bottomright' | location == 'topright') {
+  if (location == "bottomright" | location == "topright") {
     x.st.pos <- rev(x.st.pos)
   }
   label <- NULL
   if (exists("dist_unit0")) {
-    legend2 <- cbind(data[1:3, ], x = unname(x.st.pos), y = unname(st.dist),
-                     label = paste0(legend[, "text"], c("", "", "m")))
+    legend2 <- cbind(data[1:3, ],
+      x = unname(x.st.pos),
+      y = unname(st.dist),
+      label = legend[, "text"]
+    )
   } else {
-    legend2 <- cbind(data[1:3, ], x = unname(x.st.pos), y = unname(st.dist),
-                     label = paste0(legend[, "text"], c("", "", dist_unit)))
+    legend2 <- cbind(data[1:3, ],
+      x = unname(x.st.pos),
+      y = unname(st.dist),
+      label = legend[, "text"]
+    )
   }
   if (!is.null(facet.var) & !is.null(facet.lev)) {
-    for (i in 1:length(facet.var)){
+    for (i in 1:length(facet.var)) {
       if (any(class(data) == "sf")) {
-        legend2[ , facet.var[i]] <- factor(facet.lev[i],
-                                           levels(data[ , facet.var[i]][[1]]))
+        legend2[, facet.var[i]] <- factor(
+          facet.lev[i],
+          levels(data[, facet.var[i]][[1]])
+        )
       } else {
-        legend2[ , facet.var[i]] <- factor(facet.lev[i],
-                                           levels(data[ , facet.var[i]]))
+        legend2[, facet.var[i]] <- factor(
+          facet.lev[i],
+          levels(data[, facet.var[i]])
+        )
       }
     }
   } else if (!is.null(facet.var) & is.null(facet.lev)) {
     facet.levels0 <- unique(as.data.frame(data)[, facet.var])
     facet.levels <- unlist(unique(as.data.frame(data)[, facet.var]))
     legend2 <- do.call("rbind", replicate(length(facet.levels),
-                                          legend2, simplify = FALSE))
+      legend2,
+      simplify = FALSE
+    ))
     if (length(facet.var) > 1) {
       facet.levels0 <- expand.grid(facet.levels0)
       legend2[, facet.var] <-
@@ -804,10 +860,43 @@ scalebar <- function(data = NULL, location = "bottomright", dist = NULL, dist_un
   if (!st.inherit) {
     legend2 <- legend2[, c("x", "y", "label")]
   }
-  gg.legend <- geom_text(data = legend2, aes(x, y, label = label),
-                         size = st.size, color = st.color,
-                         # We must change the font family the hard way!
-                         inherit.aes = st.inherit,
-                         family = "Fira Sans Condensed")
-  return(list(gg.box1, gg.box2, gg.legend))
+  gg.legend <-
+    geom_text(
+      data = legend2,
+      aes(
+        x,
+        y,
+        label = label
+      ),
+      size = st.size,
+      color = st.color,
+      # We must change the font family the hard way!
+      inherit.aes = st.inherit,
+      family = "Fira Sans Condensed"
+    )
+  # Create unit label position
+  if (legend2$label[3] < 10) {
+    unit_pos_dist <- 0.4
+  } else {
+    unit_pos_dist <- 0.45
+  }
+  unit_pos <-
+    maptools::gcDestination(
+      lon = x,
+      lat = y,
+      bearing = 90,
+      dist = dist * unit_pos_dist,
+      dist.units = dist_unit,
+      model = model
+    )[1, 1]
+  gg.unit <-
+    annotate(
+      "text",
+      x = unit_pos,
+      y = max(legend2$y),
+      label = dist_unit,
+      family = "Fira Sans Condensed"
+    )
+
+  return(list(gg.box1, gg.box2, gg.legend, gg.unit))
 }
