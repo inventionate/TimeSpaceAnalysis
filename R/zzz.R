33cc39c3fda73eb371d158a61d08ef362cb1210a
Fabian Mundt <f.mundt@inventionate.de>
2024-09-13 15:48:52 +0200

    Add better font handling

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,14 @@
 .onLoad <- function(...) {
-    systemfonts::register_variant(
-        name = "Fira Sans Condensed Semibold",
-        family = "Fira Sans Condensed",
-        weight = c("normal", "semibold")
-    )
-    systemfonts::register_variant(
-        name = "Fira Sans Condensed Medium",
-        family = "Fira Sans Condensed",
-        weight = c("normal", "medium")
-    )
+    if (systemfonts::match_fonts("Fira Sans Condensed") |> nrow()) {
+        systemfonts::register_variant(
+            name = "Fira Sans Condensed Semibold",
+            family = "Fira Sans Condensed",
+            weight = c("normal", "semibold")
+        )
+        systemfonts::register_variant(
+            name = "Fira Sans Condensed Medium",
+            family = "Fira Sans Condensed",
+            weight = c("normal", "medium")
+        )
+    }
 }
